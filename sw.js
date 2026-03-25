50bb1fe7f83cc6b861052547ac1500c07f08b898
aiwakuwaku <parplay1872@gmail.com>
2026-03-25 11:06:35 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'golf-yardage-v7';
+const CACHE_NAME = 'golf-yardage-v8'; // バージョンをアップ
 const ASSETS = [
   'index.html',
   'manifest.json',
@@ -6,7 +6,20 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    .then(() => self.skipWaiting()) // すぐに新しい版を有効化
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
